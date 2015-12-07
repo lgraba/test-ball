26fa22b4ba790e91977af26ab56d5ed88618fa87
Logan Graba <logan@space-nav.com>
2015-12-07 14:02:41 -0700

    Finished up a little test guestbook

--- hello.go
+++ hello.go
@@ -1,42 +1,80 @@
-// hello.go
-// A Hellow World HTTP serving example application
-// Next up: Deploy a guestbook, fool
-
-package hello
+package guestbook
 
 import (
-	// "fmt"
-	"html/template"
-	"io/ioutil"
-	"net/http"
-	// "appengine"
-	// "appengine/user"
+        "html/template"
+        "net/http"
+        "time"
+
+        "appengine"
+        "appengine/datastore"
+        "appengine/user"
 )
 
-var (
-	guestbookForm []byte
-	signTemplate = template.Must(template.ParseFiles("guestbook.html"))
-)
-
+// [START greeting_struct]
+type Greeting struct {
+        Author  string
+        Content string
+        Date    time.Time
+}
+// [END greeting_struct]
 
 func init() {
-	content, err := ioutil.ReadFile("guestbookform.html")
-	if err != nil {
-		panic(err)
-	}
-	guestbookForm = content
-
-	http.HandleFunc("/", root)
-	http.HandleFunc("/entry", entry)
+        http.HandleFunc("/", root)
+        http.HandleFunc("/sign", sign)
 }
 
+// guestbookKey returns the key used for all guestbook entries.
+func guestbookKey(c appengine.Context) *datastore.Key {
+        // The string "default_guestbook" here could be varied to have multiple guestbooks.
+        return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+}
+
+// [START func_root]
 func root(w http.ResponseWriter, r *http.Request) {
-	w.Write(guestbookForm)
+        c := appengine.NewContext(r)
+        // Ancestor queries, as shown here, are strongly consistent with the High
+        // Replication Datastore. Queries that span entity groups are eventually
+        // consistent. If we omitted the .Ancestor from this query there would be
+        // a slight chance that Greeting that had just been written would not
+        // show up in a query.
+        // [START query]
+        q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+        // [END query]
+        // [START getall]
+        greetings := make([]Greeting, 0, 10)
+        if _, err := q.GetAll(c, &greetings); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+        // [END getall]
+        if err := guestbookTemplate.Execute(w, greetings); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
 }
+// [END func_root]
 
+var guestbookTemplate = template.Must(template.ParseFiles("guestbook.html"))
+
+// [START func_sign]
 func sign(w http.ResponseWriter, r *http.Request) {
-	err := signTemplate.Execute(w, r.FormValue("content"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+        c := appengine.NewContext(r)
+        g := Greeting{
+                Content: r.FormValue("content"),
+                Date:    time.Now(),
+        }
+        if u := user.Current(c); u != nil {
+                g.Author = u.String()
+        }
+        // We set the same parent key on every Greeting entity to ensure each Greeting
+        // is in the same entity group. Queries across the single entity group
+        // will be consistent. However, the write rate to a single entity group
+        // should be limited to ~1/second.
+        key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+        _, err := datastore.Put(c, key, &g)
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+        http.Redirect(w, r, "/", http.StatusFound)
 }
+// [END func_sign]
